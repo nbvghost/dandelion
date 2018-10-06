dc84aced118d298d39343ea88795c517095cd580
nbvghost <nbvghost@qq.com>
2018-10-06 09:25:05 +0800

    Signed-off-by: nbvghost <nbvghost@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# dandelion
-dandelion
+add yacco
---
+++ build.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+### BEGIN INIT INFO
+### END INIT INFO
+echo 'build app'
+export GOPATH='C:\Users\work\Documents\DandelionWork;C:\Users\work\go'
+GOOS=linux go build -ldflags '-s -w'
