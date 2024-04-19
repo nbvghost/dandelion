37783304736ae0f815fa185594ce02d9929466ad
nbvghost <nbvghost@gmail.com>
2024-04-19 13:33:31 +0800

    etcd

--- library/environments/Environments.go
+++ library/environments/Environments.go
@@ -2,25 +2,32 @@ package environments
 
 import (
 	"flag"
+	"github.com/nbvghost/dandelion/config"
 	"log"
 	_ "net/http/pprof"
+	"strings"
 )
 
 var env environments
 
 type environments struct {
 	release       bool
-	//etcdEndpoints []string
-	ListenIP      string
-	ListenPort    int
-	//etcdUsername  string
-	//etcdPassword  string
+	listenIP      string
+	listenPort    int
+	etcdEndpoints []string
+	etcdUsername  string
+	etcdPassword  string
 }
 
 func init() {
 	flag.BoolVar(&env.release, "release", true, "release")
-	flag.StringVar(&env.ListenIP, "ip", "", "ip")
-	flag.IntVar(&env.ListenPort, "port", 0, "port")
+	flag.StringVar(&env.listenIP, "ip", "", "ip")
+	flag.IntVar(&env.listenPort, "port", 0, "port")
+
+	etcdEndpoints := config.GetENV("ETCD_ENDPOINTS", "127.0.0.1:23791,127.0.0.1:23792,127.0.0.1:23793")
+	env.etcdEndpoints = strings.Split(etcdEndpoints, ",")
+	env.etcdUsername = config.GetENV("ETCD_USERNAME", "")
+	env.etcdPassword = config.GetENV("ETCD_PASSWORD", "")
 
 	/*etcdUsername, ok := os.LookupEnv("ETCD_USERNAME")
 	if !ok {
@@ -47,19 +54,19 @@ func init() {
 }
 func Print() {
 	log.Println("FLAG release", env.release)
-	log.Println("FLAG ip", env.ListenIP)
-	log.Println("FLAG port", env.ListenPort)
+	log.Println("FLAG ip", env.listenIP)
+	log.Println("FLAG port", env.listenPort)
 }
 func IP() string {
-	return env.ListenIP
+	return env.listenIP
 }
 func Port() int {
-	return env.ListenPort
+	return env.listenPort
 }
 func Release() bool {
 	return env.release
 }
-/*func EtcdEndpoints() []string {
+func EtcdEndpoints() []string {
 	return env.etcdEndpoints
 }
 func EtcdUsername() string {
@@ -68,4 +75,3 @@ func EtcdUsername() string {
 func EtcdPassword() string {
 	return env.etcdPassword
 }
-*/
