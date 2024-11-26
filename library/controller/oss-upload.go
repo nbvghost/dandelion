5dc229de024dd61c3449b8a9be35e35774b0b41c
nbvghost <nbvghost@gmail.com>
2024-11-26 09:07:14 +0800

    addd

---
+++ library/controller/oss-upload.go
@@ -0,0 +1,152 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/nbvghost/dandelion/config"
+	"github.com/nbvghost/dandelion/constrain"
+	"github.com/nbvghost/dandelion/domain/oss"
+	"github.com/nbvghost/dandelion/library/contexext"
+	"github.com/nbvghost/dandelion/library/result"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"path/filepath"
+	"strings"
+)
+
+type OSSUpload struct {
+	Get struct {
+		//TempFilename string `form:"filename"`
+		Path string `form:"path"`
+	} `method:"Get"`
+}
+
+func (m *OSSUpload) HandlePost(context constrain.IContext) (constrain.IResult, error) {
+	contextValue := contexext.FromContext(context)
+	err := contextValue.Request.ParseMultipartForm(10 * 1024 * 1024)
+	if err != nil {
+		return nil, err
+	}
+
+	var file multipart.File
+	file, fileHeader, err := contextValue.Request.FormFile("file")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileByte, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	ext := filepath.Ext(fileHeader.Filename)
+	/*if strings.EqualFold(filepath.Ext(fileHeader.Filename), ".mp4") {
+
+	} else {
+
+	}
+	buffer := bytes.NewBuffer(nil)
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, errors.Errorf("不支持的图片格式，请确认图片是否合格,%s", err.Error())
+	}
+	err = jpeg.Encode(buffer, img, nil)
+	if err != nil {
+		return nil, errors.Errorf("不支持的图片格式，请确认图片是否合格,%s", err.Error())
+	}*/
+
+	//fileByte := buffer.Bytes()
+	fileName, err := oss.CreateTempWithExt(fileByte, strings.ToLower(ext))
+	if err != nil {
+		return nil, err
+	}
+	return result.NewData(map[string]any{"Path": fileName}), nil
+}
+
+func (m *OSSUpload) Handle(context constrain.IContext) (constrain.IResult, error) {
+	if strings.HasPrefix(m.Get.Path, oss.TempFilePrefix) {
+		b, err := oss.GetTempFile(m.Get.Path)
+		if err != nil {
+			return nil, err
+		}
+		return &result.ImageBytesResult{
+			Data:        b,
+			ContentType: "",
+			Filename:    m.Get.Path,
+		}, nil
+	} else {
+		return m.ossLoad(context)
+	}
+
+}
+
+func (m *OSSUpload) ossLoad(ctx constrain.IContext) (constrain.IResult, error) {
+	contextValue := contexext.FromContext(ctx)
+	server, err := ctx.Etcd().SelectInsideServer(config.MicroServerOSS) //config.MicroServerOSS.SelectInsideServer() //ctx.SelectInsideServer(config.MicroServerOSS)
+	if err != nil {
+		return nil, err
+	}
+	ossUrl, err := url.Parse(fmt.Sprintf("http://%s", server))
+	if err != nil {
+		return nil, err
+	}
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(ossUrl)
+	reverseProxy.Director = func(request *http.Request) {
+
+		targetQuery := ossUrl.RawQuery
+		director := func(req *http.Request) {
+			req.URL.Scheme = ossUrl.Scheme
+			req.URL.Host = ossUrl.Host
+			req.URL.Path, req.URL.RawPath = joinURLPath(ossUrl, req.URL)
+			if targetQuery == "" || req.URL.RawQuery == "" {
+				req.URL.RawQuery = targetQuery + req.URL.RawQuery
+			} else {
+				req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+			}
+			if _, ok := req.Header["User-Agent"]; !ok {
+				// explicitly disable User-Agent so it's not set to default value
+				req.Header.Set("User-Agent", "")
+			}
+			path := filepath.ToSlash(filepath.Join("/assets", m.Get.Path))
+			req.URL.Path = path
+		}
+		director(request)
+	}
+	reverseProxy.ServeHTTP(contextValue.Response, contextValue.Request)
+
+	return &result.NoneResult{}, nil
+}
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
---
+++ library/controller/restful-page.go
@@ -0,0 +1,176 @@
+package controller
+
+import (
+	"errors"
+	"fmt"
+	"github.com/nbvghost/dandelion/constrain"
+	"github.com/nbvghost/dandelion/entity"
+	"github.com/nbvghost/dandelion/library/dao"
+	"github.com/nbvghost/dandelion/library/db"
+	"github.com/nbvghost/dandelion/library/result"
+	"log"
+	"reflect"
+)
+
+var conditions = map[string]any{"like": true, "in": true, "=": true, "!=": true, ">": true, "<": true}
+
+type Query struct {
+	Field     string //验证
+	Condition string //验证,=,!=,>,<,or,
+	Value     any
+}
+
+type RestfulPage[T IAdminTable] struct {
+	Admin T `mapping:""`
+	Post  struct {
+		Model    string
+		PageNo   int
+		PageSize int
+		Sort     []dao.Sort
+		Query    map[string]any //and,or
+	} `method:"post"`
+}
+
+type Where struct {
+	Query any
+	Args  []any
+}
+
+func (m *RestfulPage[T]) condition(ctx constrain.IContext, key string, tableType reflect.Type) ([]Where, error) {
+	queryValue := m.Post.Query[key] //
+	valueType := reflect.TypeOf(queryValue)
+	log.Println(queryValue, valueType.Kind())
+
+	whereList := make([]Where, 0)
+
+	var queryList []Query
+	if valueType.Kind() == reflect.Map {
+		qMap, ok := queryValue.(map[string]any)
+		if !ok {
+			return nil, nil
+		}
+		for s, a := range qMap {
+			queryList = append(queryList, Query{
+				Field:     s,
+				Condition: "=",
+				Value:     a,
+			})
+		}
+	}
+	if valueType.Kind() == reflect.Slice {
+		var ok bool
+		var arr []map[string]any
+		arr, ok = queryValue.([]map[string]any)
+		if !ok {
+			return nil, errors.New("无效的查询参数")
+		}
+		for i := range arr {
+			qMap := arr[i]
+			queryList = append(queryList, Query{
+				Field:     qMap["Field"].(string),
+				Condition: qMap["Condition"].(string),
+				Value:     qMap["Value"],
+			})
+		}
+	}
+
+	for index := range queryList {
+		query := queryList[index] //[]Query,map[string]any
+		{
+			if query.Value == nil {
+				//空值不处理
+				continue
+			}
+			valueValue := reflect.ValueOf(query.Value)
+			if valueValue.IsZero() {
+				//0值不处理
+				continue
+			}
+
+			field, has := tableType.FieldByName(query.Field)
+			if !has {
+				//不存在的字段不处理，子struct不处理
+				continue
+				//return nil, errors.New(fmt.Sprintf("not find field <%s>", query.Field))
+			}
+			log.Println(field.Type.Kind())
+			if field.Type.Kind() == reflect.Struct {
+				continue
+			}
+			if field.Type.Kind() == reflect.Array || field.Type.Kind() == reflect.Slice {
+				continue
+			}
+			if _, has := conditions[query.Condition]; has == false {
+				//不存在的条件不处理
+				//return nil, errors.New(fmt.Sprintf("not find condition <%s>", query.Condition))
+				continue
+			}
+		}
+
+		if query.Condition == "like" {
+			whereList = append(whereList, Where{
+				Query: fmt.Sprintf(`"%s" ilike ?`, query.Field),
+				Args:  []any{query.Value},
+			})
+		} else {
+			whereList = append(whereList, Where{
+				Query: fmt.Sprintf(`"%s" %s ?`, query.Field, query.Condition),
+				Args:  []any{query.Value},
+			})
+		}
+	}
+
+	return whereList, nil
+}
+
+func (m *RestfulPage[T]) HandlePost(ctx constrain.IContext) (constrain.IResult, error) {
+	table, err := entity.GetModel(m.Post.Model)
+	if err != nil {
+		return nil, err
+	}
+
+	tableType := reflect.TypeOf(table).Elem()
+
+	d := db.Orm().Table(m.Post.Model) //dao.Find(db.Orm(), table)
+
+	wheres, err := m.condition(ctx, "and", tableType)
+	if err != nil {
+		return nil, err
+	}
+	for i := range wheres {
+		where := wheres[i]
+		d.Where(where.Query, where.Args...)
+	}
+
+	wheres, err = m.condition(ctx, "or", tableType)
+	if err != nil {
+		return nil, err
+	}
+	for i := range wheres {
+		where := wheres[i]
+		d.Or(where.Query, where.Args...)
+	}
+
+	_, exist := tableType.FieldByName("OID")
+	if exist {
+		d.Where(`"OID"=?`, m.Admin.GetOID())
+	}
+
+	index := m.Post.PageNo - 1
+	if index < 0 {
+		index = 0
+	}
+	var total int64
+	d.Limit(m.Post.PageSize).Offset(index * m.Post.PageSize).Count(&total)
+	for i := range m.Post.Sort {
+		sort := m.Post.Sort[i]
+		d.Order(fmt.Sprintf(`"%s" %s`, sort.SortField(), sort.SortMethod()))
+	}
+
+	var list = reflect.New(reflect.SliceOf(reflect.TypeOf(table)))
+	d.Find(list.Interface())
+	return result.NewData(result.NewPagination(index+1, m.Post.PageSize, total, list.Interface())), nil
+}
+func (m *RestfulPage[T]) Handle(ctx constrain.IContext) (constrain.IResult, error) {
+	return nil, nil
+}
