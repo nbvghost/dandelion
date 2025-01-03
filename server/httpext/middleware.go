dedf498336d5a6fce9ae8d83e104b2dd593cd22b
nbvghost <nbvghost@gmail.com>
2025-01-04 03:06:49 +0800

    middleware.go

--- server/httpext/middleware.go
+++ server/httpext/middleware.go
@@ -86,8 +86,9 @@ func (m *httpMiddleware) bindData(apiHandler any, ctx constrain.IContext, contex
 		} else {
 			log.Println(fmt.Sprintf("%s %s %s %s", m.serverName, contextValue.Request.URL.Path, contextValue.Request.Method, string(body)))
 		}
-		contextValue.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 	}
+
+	contextValue.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 
 	var bodyValue reflect.Value
 	var hasBodyField bool
@@ -141,7 +142,6 @@ func (m *httpMiddleware) bindData(apiHandler any, ctx constrain.IContext, contex
 		ctx.Logger().With(zap.Error(err))
 		return err
 	}
-	contextValue.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 	return nil
 }
 
