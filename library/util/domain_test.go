d09a7d956e22b041453cf7845418bacb487d0b0c
nbvghost <nbvghost@gmail.com>
2026-02-09 01:50:03 +0800

    domain_test.go

--- library/util/domain_test.go
+++ library/util/domain_test.go
@@ -16,15 +16,16 @@ func TestParseDomain(t *testing.T) {
 		want1 string
 	}{
 		{name: "#1", args: args{host: "bem.usokay.com"}, want: []string{"bem"}, want1: "usokay.com"},
+		{name: "#2", args: args{host: "asset.sites.ink"}, want: []string{"bem"}, want1: "usokay.com"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, got1 := ParseDomain(tt.args.host)
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ParseDomain() got = %v, want %v", got, tt.want)
+				t.Errorf("ParseDomain() got = %v,%v, want %v", got, got1, tt.want)
 			}
 			if got1 != tt.want1 {
-				t.Errorf("ParseDomain() got1 = %v, want %v", got1, tt.want1)
+				t.Errorf("ParseDomain() got = %v,%v, want %v", got, got1, tt.want1)
 			}
 		})
 	}
