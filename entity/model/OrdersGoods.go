67d2f9cf58d54e9caa8c9ffd46633c215666bd24
nbvghost <nbvghost@gmail.com>
2026-03-15 00:40:40 +0800

    OrdersGoods.go

--- entity/model/OrdersGoods.go
+++ entity/model/OrdersGoods.go
@@ -54,26 +54,27 @@ func (OrdersGoodsRefund) TableName() string {
 
 type OrdersGoods struct {
 	dao.Entity
-	OID             dao.PrimaryKey                  `gorm:"column:OID"`
-	OrdersGoodsNo   string                          `gorm:"column:OrdersGoodsNo;unique"` //
-	Status          OrdersGoodsStatus               `gorm:"column:Status"`               //OGAskRefund，OGRefundNo，OGRefundOk，OGRefundInfo，OGRefundComplete
-	RefundID        dao.PrimaryKey                  `gorm:"column:RefundID"`             //RefundInfo json 退款退货信息
-	OrdersID        dao.PrimaryKey                  `gorm:"column:OrdersID;index"`       //
-	GoodsID         dao.PrimaryKey                  `gorm:"column:GoodsID"`
-	SpecificationID dao.PrimaryKey                  `gorm:"column:SpecificationID"`
-	Goods           Goods                           `gorm:"column:Goods;serializer:json;type:json"`         //josn,快照使用
-	Specification   Specification                   `gorm:"column:Specification;serializer:json;type:json"` //json,快照使用
-	GoodsSkus       Array[GoodsSku]                 `gorm:"column:GoodsSkus;type:json"`                     //json,只是对Specification进行分组选择使用的
-	Discounts       sqltype.Array[sqltype.Discount] `gorm:"column:Discounts;type:json"`
-	Quantity        uint                            `gorm:"column:Quantity"`                          //数量
-	CostPrice       decimal.Decimal                 `gorm:"column:CostPrice;type:numeric(24,6)"`      //单价-原价
-	SellPrice       decimal.Decimal                 `gorm:"column:SellPrice;type:numeric(24,6)"`      //单价-销售价
-	Currency        Currency                        `gorm:"column:Currency"`                          //销售价-货币
-	TotalBrokerage  decimal.Decimal                 `gorm:"column:TotalBrokerage;type:numeric(24,6)"` //总佣金
-	Error           string                          `gorm:"column:Error"`                             //
-	Image           string                          `gorm:"column:Image"`                             //Deprecated:当前规格的图片，如果规格没有图片，使用产品主图的第一张
-	TotalCostPrice  decimal.Decimal                 `gorm:"column:TotalCostPrice;type:numeric(24,6)"` //总价-原价
-	TotalSellPrice  decimal.Decimal                 `gorm:"column:TotalSellPrice;type:numeric(24,6)"` //总价-销售价
+	OID                dao.PrimaryKey                  `gorm:"column:OID"`
+	OrdersGoodsNo      string                          `gorm:"column:OrdersGoodsNo;unique"` //
+	Status             OrdersGoodsStatus               `gorm:"column:Status"`               //OGAskRefund，OGRefundNo，OGRefundOk，OGRefundInfo，OGRefundComplete
+	RefundID           dao.PrimaryKey                  `gorm:"column:RefundID"`             //RefundInfo json 退款退货信息
+	OrdersID           dao.PrimaryKey                  `gorm:"column:OrdersID;index"`       //
+	GoodsID            dao.PrimaryKey                  `gorm:"column:GoodsID"`
+	SpecificationID    dao.PrimaryKey                  `gorm:"column:SpecificationID"`
+	Goods              Goods                           `gorm:"column:Goods;serializer:json;type:json"`         //josn,快照使用
+	Specification      Specification                   `gorm:"column:Specification;serializer:json;type:json"` //json,快照使用
+	GoodsSkus          Array[GoodsSku]                 `gorm:"column:GoodsSkus;type:json"`                     //json,只是对Specification进行分组选择使用的
+	Discounts          sqltype.Array[sqltype.Discount] `gorm:"column:Discounts;type:json"`
+	Quantity           uint                            `gorm:"column:Quantity"`                          //数量
+	CostPrice          decimal.Decimal                 `gorm:"column:CostPrice;type:numeric(24,6)"`      //单价-原价
+	SellPrice          decimal.Decimal                 `gorm:"column:SellPrice;type:numeric(24,6)"`      //单价-销售价
+	Currency           Currency                        `gorm:"column:Currency"`                          //销售价-货币
+	TotalBrokerage     decimal.Decimal                 `gorm:"column:TotalBrokerage;type:numeric(24,6)"` //总佣金
+	Error              string                          `gorm:"column:Error"`                             //
+	Image              string                          `gorm:"column:Image"`                             //Deprecated:当前规格的图片，如果规格没有图片，使用产品主图的第一张
+	TotalCostPrice     decimal.Decimal                 `gorm:"column:TotalCostPrice;type:numeric(24,6)"` //总价-原价
+	TotalSellPrice     decimal.Decimal                 `gorm:"column:TotalSellPrice;type:numeric(24,6)"` //总价-销售价
+	CustomsDeclaration bool                            `gorm:"column:CustomsDeclaration"`                //报关
 	//TotalNetWeight  uint                            `gorm:"column:TotalNetWeight"` //总的净重,包含所有的数量,在收到货盘点得出
 	//Pictures        sqltype.Array[sqltype.Image]    `gorm:"column:Pictures;type:JSON"` //规格的多张图片,当前规格的图片，如果规格没有图片，使用产品主图的第一张
 	//SpecificationID uint `gorm:"column:SpecificationID"`             //
